cf5fdb0c0231dec4b848f24529277dd46af5fb68
kalpadas599 <kalpadas599@gmail.com>
2024-12-02 00:29:15 +0530

    first commit

---
+++ speech.py
@@ -0,0 +1,21 @@
+#text to speech converter
+import pyttsx3
+engine = pyttsx3.init()
+answer = input("what you want to convert into speech:")
+#engine.setProperty("rate", 150)
+engine.say(answer)
+engine.runAndWait()
+
+""" #speech to text converter
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("speak anything")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("you said : {}".format(text))
+    except:
+        print("sorry could not recognize your voice")
+   """  
+
